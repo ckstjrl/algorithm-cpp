a1246b8e0abfd5d2aa04f1ec952c6fca13295018
ckstjrl <qkrckstjr7876@gmail.com>
2026-01-14 23:29:56 +0900

    solved(BOJ10951 / B5): A+B - 4

---
+++ boj/bronze/BOJ10951.cpp
@@ -0,0 +1,19 @@
+// BOJ 10951. A+B - 4 / B5
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int A, B;
+	while (cin >> A >> B) {
+		cout << A + B << "\n";
+	}
+}
+
+/*
+while (cin >> A >> B)
+input이 들어오는 동안 실행
+*/
